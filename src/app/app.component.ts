02f5cf6243538212cf86406159e0cd2118f929e7
hakan2259 <hakan.sandal9@hotmail.com>
2022-04-12 06:02:27 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private accountService: AccountService, private router: Router) {}
   title = 'shop';
+
+  isLoggedIn(): boolean {
+    return this.accountService.isLoggedIn();
+  }
+  logOut() {
+    this.accountService.logOut();
+    this.router.navigate(['login']);
+  }
 }
